beed31ffdc308a5bca164801b84ca047925a9f17
Werner Palfinger <werner.palfinger@profactor.at>
2019-12-14 15:02:32 +0100

    , pure_c=False

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="cv-plot:shared")
+    builder.add_common_builds(shared_option_name="cv-plot:shared", pure_c=False)
     builder.run()
