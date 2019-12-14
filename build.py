dc5fb950c5fe93e98b990466dbc93269faeff58a
Werner Palfinger <werner.palfinger@profactor.at>
2019-12-14 15:35:03 +0100

    options.copy added

--- build.py
+++ build.py
@@ -4,7 +4,6 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="cv-plot:shared", pure_c=False)
-    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") #opencv requires c++11
     
     filtered_builds = []
     for settings, options, env_vars, build_requires, reference in builder.items:
@@ -12,10 +11,12 @@ if __name__ == "__main__":
         if settings["compiler.libcxx"] == "libstdc++": #opencv requires c++11
             continue
             
+        options = options.copy()
         options["opencv:shared"] = False
         filtered_builds.append([settings, options, env_vars, build_requires, reference])
         
         if options["cv-plot:shared"] == True:
+            options = options.copy()
             options["opencv:shared"] = True
             filtered_builds.append([settings, options, env_vars, build_requires, reference])
             
