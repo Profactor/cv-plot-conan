7aaf7f6bfe8793bbeb5ededd3e186d3d550069fe
Werner Palfinger <werner.palfinger@profactor.at>
2019-12-14 15:13:55 +0100

    c++11 only, opencv shared and static

--- build.py
+++ build.py
@@ -4,4 +4,21 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="cv-plot:shared", pure_c=False)
+    builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") #opencv requires c++11
+    
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        
+        if settings["compiler.libcxx"] == "libstdc++": #opencv requires c++11
+            continue
+            
+        options["opencv:shared"] = False
+        filtered_builds.append([settings, options, env_vars, build_requires, reference])
+        
+        if settings["cv-plot:shared"] == True:
+            options["opencv:shared"] = True
+            filtered_builds.append([settings, options, env_vars, build_requires, reference])
+            
+    builder.builds = filtered_builds
+    
     builder.run()
