e098689c258c9df3640ada98577967532ea3a029
Werner Palfinger <werner.palfinger@profactor.at>
2019-12-15 01:24:06 +0100

    only header-only, no defaults

--- build.py
+++ build.py
@@ -3,19 +3,21 @@ import os
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="cv-plot:shared", pure_c=False)
     
-    filtered_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        
-        if "compiler.libcxx" in settings and settings["compiler.libcxx"] == "libstdc++": #opencv requires c++11
-            continue
-            
-        filtered_builds.append([settings, options, env_vars, build_requires, reference])
-    
-    builder.builds = filtered_builds
-        
     if 'CVPLOT_HEADER_ONLY' in os.environ:
         builder.add(options={"header_only": True})
+    else:
+        builder.add_common_builds(shared_option_name="cv-plot:shared", pure_c=False)
+        
+        filtered_builds = []
+        for settings, options, env_vars, build_requires, reference in builder.items:
+            
+            if "compiler.libcxx" in settings and settings["compiler.libcxx"] == "libstdc++": #opencv requires c++11
+                continue
+                
+            filtered_builds.append([settings, options, env_vars, build_requires, reference])
+        
+        builder.builds = filtered_builds
+        
     
     builder.run()
