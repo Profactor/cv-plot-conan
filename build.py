c28d12595a0df156dbb852d8c1cc1edb2b315527
Werner Palfinger <werner.palfinger@profactor.at>
2019-12-15 21:43:25 +0100

    CvPlot in build.py

--- build.py
+++ build.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     
     if 'CVPLOT_HEADER_ONLY' in os.environ:
-        builder.add(options={"cv-plot:header_only": True})
+        builder.add(options={"CvPlot:header_only": True})
     else:
-        builder.add_common_builds(shared_option_name="cv-plot:shared", pure_c=False)
+        builder.add_common_builds(shared_option_name="CvPlot:shared", pure_c=False)
         
         filtered_builds = []
         for settings, options, env_vars, build_requires, reference in builder.items:
