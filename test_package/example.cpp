a7f815b35eb8f7e3ab462d6b74ff32edfe56d64e
Werner Palfinger <werner.palfinger@profactor.at>
2020-01-13 13:22:29 +0100

    added hello

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -6,5 +6,6 @@ using namespace CvPlot;
 int main() {
 	Axes axes = plot(std::vector<double>{ 3, 3, 4, 6, 4, 3 }, "-o");
 	cv::Mat mat = axes.render(400, 600);
+    std::cout << "hello from package test!" << std::endl;
     return 0;
 }
